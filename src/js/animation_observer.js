654bdabfe24b2bca62cba597da3ae3b29696869b
Krishan <kmkrider1999@gmail.com>
2023-12-07 22:05:30 +0530

    Added Services

--- src/js/animation_observer.js
+++ src/js/animation_observer.js
@@ -41,3 +41,69 @@ for (let i = 0; i < steps.length; i++) {
   const step = steps[i];
   steps_observer.observe(step);
 }
+
+// Doctors Intro animation observer
+
+const intro_text = document.querySelectorAll(".intro_text");
+
+const intro_text_observer = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("text_scroll_animation");
+      } else {
+        entry.target.classList.remove("text_scroll_animation");
+      }
+    });
+  },
+  { threshold: 0.5 }
+);
+
+for (let i = 0; i < intro_text.length; i++) {
+  const text = intro_text[i];
+  intro_text_observer.observe(text);
+}
+
+// Successful Deliveries animation observer
+
+const deliveries = document.querySelectorAll(".delivery");
+
+const deliveries_observer = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("delivery_scroll_animation");
+      } else {
+        entry.target.classList.remove("delivery_scroll_animation");
+      }
+    });
+  },
+  { threshold: 0.5 }
+);
+
+for (let i = 0; i < deliveries.length; i++) {
+  const delivery = deliveries[i];
+  deliveries_observer.observe(delivery);
+}
+
+// Services animation observer
+
+const services = document.querySelectorAll(".service");
+
+const services_observer = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("service_scroll_animation");
+      } else {
+        entry.target.classList.remove("service_scroll_animation");
+      }
+    });
+  },
+  { threshold: 0.5 }
+);
+
+for (let i = 0; i < services.length; i++) {
+  const service = services[i];
+  services_observer.observe(service);
+}
